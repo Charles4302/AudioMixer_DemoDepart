07cdab06314af25d2108cbc68406916a94378ae6
Charles4302 <143675094+Charles4302@users.noreply.github.com>
2024-04-02 17:02:24 -0400

    Faire l'exercice

---
+++ Assets/Scripts/GestionnaireAudio.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class GestionnaireAudio : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixer audioMixer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //audioMixer.SetFloat("VolumeMusique", -30f);
+    }
+
+    public void AjusteVolumeMusique(float volume){
+        audioMixer.SetFloat("VolumeMusique", ConvertToLogarithmique(volume) );
+    }
+
+    public void AjusteVolumeFX(float volume){
+        audioMixer.SetFloat("VolumeFX", ConvertToLogarithmique(volume) );
+    }
+
+
+    private float ConvertToLogarithmique(float volume){
+        return Mathf.Log10(volume) * 20;
+    }
+
+}
